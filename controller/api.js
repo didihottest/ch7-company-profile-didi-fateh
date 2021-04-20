9dc87d2f36ebbfda85e3c13092e0526e14fa39e1
didihottest <didihottest@gmail.com>
2021-04-20 19:23:48 +0700

    add new api controller to route

--- controller/api.js
+++ controller/api.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 const {About, Portfolio, Employee} = require('./../model/api')
-const multer = require('multer');
 // Get request raw json from postman / api
 app.use(express.json());
 // Get request form form-urlencoded form postman / api
@@ -10,29 +9,29 @@ app.use(express.urlencoded({ extended: true }));
 
 // display API for all collections
 // display all about entry from database
-exports.getAbouts = async (req, res, next) => {
-  await About.find().exec((err, about)=>{
+exports.getAbouts =  (req, res, next) => {
+  About.find().exec((err, about)=>{
     res.json(about)
   })
 }
 
 // display all portfolio entry from database
-exports.getPortfolios = async (req, res, next) => {
-  await Portfolio.find().exec((err, portfolios)=>{
+exports.getPortfolios = (req, res, next) => {
+  Portfolio.find().exec((err, portfolios)=>{
     res.json(portfolios)
   })
 }
 
 // display all employees entry from database
-exports.getEmployees = async (req, res, next) => {
-  await Employee.find().exec((err, employees)=>{
+exports.getEmployees =  (req, res, next) => {
+  Employee.find().exec((err, employees)=>{
     res.json(employees)
   })
 }
 
 //all endpoint for add new data to collections
 // add new entry to about collection
-exports.addAbout = (multer().none(), async (req, res, next) => {
+exports.addAbout = (req, res, next) => {
   const {year, title, description, imageURL} = req.body;
   const newAbout = new About({
     year: year,
@@ -43,12 +42,14 @@ exports.addAbout = (multer().none(), async (req, res, next) => {
   newAbout.save((err) => {
     if (err) {
       console.error(err)
+    } else {
+      res.json(newAbout)
     }
   })
-})
+}
 
 // add new entry to portfolios collection
-exports.addPortfolio = (multer().none(), async (req, res, next) => {
+exports.addPortfolio = (req, res, next) => {
   const {title, description, imageURL} = req.body;
   const newPortfolio = new Portfolio({
     title: title,
@@ -58,12 +59,14 @@ exports.addPortfolio = (multer().none(), async (req, res, next) => {
   newPortfolio.save((err) => {
     if (err) {
       console.error(err)
+    } else {
+      res.json(newPortfolio)
     }
   })
-})
+}
 
 // add new entry to employees collections
-exports.addEmployee = (multer().none(), async (req, res, next) => {
+exports.addEmployee = (req, res, next) => {
   const {fullname, position, imageURL, twitterURL, facebookURL, linkedinURL} = req.body;
   const newEmployee = new Employee({
     fullname: fullname,
@@ -76,6 +79,8 @@ exports.addEmployee = (multer().none(), async (req, res, next) => {
   newEmployee.save((err) => {
     if (err) {
       console.error(err)
+    } else {
+      res.json(newEmployee)
     }
   })
-})
+}
--- routes/router.js
+++ routes/router.js
@@ -1,18 +1,28 @@
 // use express module
 const express = require('express');
 const app = express();
+const multer = require('multer')
 const router = express.Router();
 // Get request raw json from postman / api
 app.use(express.json());
 // Get request form form-urlencoded form postman / api
 app.use(express.urlencoded({ extended: true }));
 const {getIndex} = require('./../controller/index');
-const {getAbouts, getPortfolios, getEmployees} = require('./../controller/api');
+const {
+  getAbouts, getPortfolios, getEmployees, 
+  addAbout, addPortfolio, addEmployee} = require('./../controller/api');
 
-// api
+// api get
 router.get('/api/abouts', getAbouts);
 router.get('/api/portfolios', getPortfolios);
 router.get('/api/employees', getEmployees);
+// api post new data
+router.post('/api/addabout', multer().none(), addAbout);
+router.post('/api/addportfolio', multer().none(), addPortfolio);
+router.post('/api/addemployee', multer().none(), addEmployee);
+
+
+
 
 router.get("/", getIndex);
 
