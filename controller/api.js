69d1d35dc078aec0b7095e7363328fe6a5ebb923
didihottest <didihottest@gmail.com>
2021-04-20 19:11:48 +0700

    add new entry endpoint

--- controller/api.js
+++ controller/api.js
@@ -7,6 +7,8 @@ const multer = require('multer');
 app.use(express.json());
 // Get request form form-urlencoded form postman / api
 app.use(express.urlencoded({ extended: true }));
+
+// display API for all collections
 // display all about entry from database
 exports.getAbouts = async (req, res, next) => {
   await About.find().exec((err, about)=>{
@@ -28,3 +30,52 @@ exports.getEmployees = async (req, res, next) => {
   })
 }
 
+//all endpoint for add new data to collections
+// add new entry to about collection
+exports.addAbout = (multer().none(), async (req, res, next) => {
+  const {year, title, description, imageURL} = req.body;
+  const newAbout = new About({
+    year: year,
+    title: title,
+    description: description,
+    imageURL: imageURL
+  })
+  newAbout.save((err) => {
+    if (err) {
+      console.error(err)
+    }
+  })
+})
+
+// add new entry to portfolios collection
+exports.addPortfolio = (multer().none(), async (req, res, next) => {
+  const {title, description, imageURL} = req.body;
+  const newPortfolio = new Portfolio({
+    title: title,
+    description: description,
+    imageURL: imageURL
+  })
+  newPortfolio.save((err) => {
+    if (err) {
+      console.error(err)
+    }
+  })
+})
+
+// add new entry to employees collections
+exports.addEmployee = (multer().none(), async (req, res, next) => {
+  const {fullname, position, imageURL, twitterURL, facebookURL, linkedinURL} = req.body;
+  const newEmployee = new Employee({
+    fullname: fullname,
+    position: position,
+    imageURL: imageURL,
+    twitterURL: twitterURL,
+    facebookURL: facebookURL,
+    linkedinURL: linkedinURL,
+  })
+  newEmployee.save((err) => {
+    if (err) {
+      console.error(err)
+    }
+  })
+})
